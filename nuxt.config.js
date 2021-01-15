533e6cc30d8a9de276516903193881de905e4e4a
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-15 21:38:34 +0700

    Global Vusx Validation

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    './plugins/mixins/user.js'
+    './plugins/mixins/user.js',
+    './plugins/axios.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
