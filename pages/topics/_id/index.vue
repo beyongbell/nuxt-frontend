64ae895010aa9d573895dc4c6651f2113a8863dd
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-17 21:49:25 +0700

    Single Topic Page

--- pages/topics/_id/index.vue
+++ pages/topics/_id/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="container">
-      <h2> Single Topic Page </h2>
-      <pre>
-          {{ topic }}
-      </pre>
-  </div>
+   <div class="container">
+        <h2> Single Topic Page </h2>
+        <div class="bg-light my-5 p-4">
+            <h2 class="display-3"> {{ topic.title }} </h2>
+            <p class="text-muted"> {{ topic.created_at }} by {{ topic.user.name }} </p>
+            <div v-for="(content, index) in topic.posts" :key="index" class="ms-5 content p-3">
+                {{ content.body }}
+                <p class="text-muted"> {{ content.created_at }} by {{ content.user.name }} </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
